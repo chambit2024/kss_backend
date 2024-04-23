fc1cc53b27ebce18fbb8f0928b126d9ad27dd02f
SeungHo0422 <songseungho9258@gmail.com>
2024-04-23 17:57:11 +0900

    feat: add initial source files

---
+++ src/main/java/com/emptyseat/kss/KssApplication.java
@@ -0,0 +1,13 @@
+package com.emptyseat.kss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KssApplication.class, args);
+	}
+
+}
