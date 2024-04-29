0c436fea19f6744e4e08715928098852d62d83f2
SeungHo0422 <songseungho9258@gmail.com>
2024-04-29 17:47:35 +0900

    feat: add swagger configuration

---
+++ src/main/java/com/emptyseat/kss/global/config/SwaggerConfig.java
@@ -0,0 +1,15 @@
+package com.emptyseat.kss.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "KSS API Documentation",
+                description = "KSS API 명세서입니다.",
+                version = "v1"
+        ))
+@Configuration
+public class SwaggerConfig {
+}
