3273a7da52c0cf9c3b26aaca136e1227099cfedf
SeungHo0422 <songseungho9258@gmail.com>
2024-05-11 00:03:09 +0900

    feat: add test code of RedisConfig and RedisService

---
+++ src/test/java/com/emptyseat/kss/global/redis/RedisConfigTest.java
@@ -0,0 +1,15 @@
+package com.emptyseat.kss.global.redis;
+
+import com.emptyseat.kss.KssApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+@ContextConfiguration(classes = KssApplication.class)
+class RedisConfigTest {
+
+}
