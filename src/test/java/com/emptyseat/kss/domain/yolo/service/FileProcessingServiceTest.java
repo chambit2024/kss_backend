077e9bd81dceee11e8cac1723e3cd559470d9acc
SeungHo0422 <songseungho9258@gmail.com>
2024-06-07 03:47:21 +0900

    rename: change filepath

--- src/test/java/com/emptyseat/kss/domain/yolo/service/FileProcessingServiceTest.java
+++ src/test/java/com/emptyseat/kss/domain/yolo/service/FileProcessingServiceTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class FileProcessingServiceTest {
 
     private final FileProcessingService fileProcessingService = new FileProcessingService();
-    private final String mockFilePath = "/Users/seungho/Projects/kss_backend/src/main/resources/mock/mockdata_1.txt";
+    private final String mockFilePath = "/Users/seungho/Projects/kss_backend/src/main/resources/mock/chambit_test_mini_1.txt";
     @Test
     void processFile() {
         assertDoesNotThrow(() -> fileProcessingService.processFile(mockFilePath));
