b93670e95d30432728e6950b768c896c54fffc93
SeungHo0422 <songseungho9258@gmail.com>
2024-06-08 02:12:06 +0900

    feat: add box sort feature

--- src/main/java/com/emptyseat/kss/domain/yolo/util/BoxUtil.java
+++ src/main/java/com/emptyseat/kss/domain/yolo/util/BoxUtil.java
@@ -2,9 +2,14 @@ package com.emptyseat.kss.domain.yolo.util;
 
 import com.emptyseat.kss.domain.yolo.entity.Box;
 import com.emptyseat.kss.domain.yolo.service.CheckSeatService;
+import java.util.Comparator;
+import java.util.List;
 
 public class BoxUtil {
 
+    /**
+     * box1과 box2가 겹치는 지에 대한 여부 확인
+     */
     public boolean isColliding(Box box1, Box box2) {
         // 아래 조건들 중 하나라도 해당 시 충돌 X
         return !(box1.getCenter_x() + box1.getWidth()/2 < box2.getCenter_x() - box2.getWidth()/2
@@ -32,6 +37,23 @@ public class BoxUtil {
         else overlapArea = 0;
 
         return overlapArea / baseArea;
+    }
+
+    public void sortBoxesByLabelType(List<Box> boxes) {
+        boxes.sort(new Comparator<Box>() {
+            @Override
+            public int compare(Box box1, Box box2) {
+                return getOrder(box1.getLabelType().getValue()) - getOrder(box2.getLabelType().getValue());
+            }
+
+            private int getOrder(int labelType) {
+                return switch (labelType) {
+                    case 3 -> 1; // CHAIR 먼저
+                    case 6 -> 2; // 그 다음 PEOPLE
+                    default -> 3; // 나머지는 뒤로
+                };
+            }
+        });
     }
 
 }
