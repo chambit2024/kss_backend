db4f8e5eb229984570b9a43ea72796afde16fc86
SeungHo0422 <songseungho9258@gmail.com>
2024-06-09 22:44:22 +0900

    feat: add seatCount

--- src/main/java/com/emptyseat/kss/domain/yolo/service/CheckSeatService.java
+++ src/main/java/com/emptyseat/kss/domain/yolo/service/CheckSeatService.java
@@ -1,17 +1,16 @@
 package com.emptyseat.kss.domain.yolo.service;
 
 import com.emptyseat.kss.domain.yolo.entity.Box;
+import com.emptyseat.kss.domain.yolo.entity.Pos;
 import com.emptyseat.kss.domain.yolo.util.BoxUtil;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @Slf4j
@@ -20,10 +19,11 @@ public class CheckSeatService {
     private final List<Box> chairs = new ArrayList<>();
     private final List<Box> peoples = new ArrayList<>();
     private final List<Box> others = new ArrayList<>();
-
     private final BoxUtil boxUtil = new BoxUtil();
+    private final Map<Pos, Integer> seatCount = new HashMap<>();
+    private final double seatDistanceThreshold = 0.01; // 기존 자리인지의 여부를 판단하는 절댓값 기준치
+    private final double boxCollidingPercentThreshold = 30; // box들 간의 겹침 여부를 판단하는 기준값 (%)
 
-    // TODO: count 기록하는 기능 추가
 
     public CheckSeatService(List<Box> boxList) {
         for (Box box: boxList) {
@@ -37,22 +37,43 @@ public class CheckSeatService {
     }
 
     public void FindHoggedSeat() {
+        boolean isSeatExist = false;
+        log.info("[BEFORE] seatCount : " + seatCount.toString());
         for (Box chair: chairs) {
+            Pos currentChairPos = new Pos(chair.getCenter_x(), chair.getCenter_y());
+            for (Pos seatPos: seatCount.keySet()) {
+                if (boxUtil.isSameSeat(currentChairPos, seatPos, seatDistanceThreshold)) {
+                    // 기존 좌석 취급. 좌표 업데이트 필요
+                    isSeatExist = true;
+                    int currentSeatCount = seatCount.get(seatPos);
+                    seatCount.remove(seatPos);
+                    seatCount.put(currentChairPos, currentSeatCount);
+                    log.info("seatCount 업데이트 : " + seatCount.get(currentChairPos));
+                    break;
+                }
+            }
+            if (!isSeatExist) {
+                seatCount.put(currentChairPos, 0);
+            }
             for (Box people: peoples) {
-                if (boxUtil.isColliding(chair, people)) {
+                if (boxUtil.overlap_percent(chair, people) > boxCollidingPercentThreshold) {
                     // 사석화 X. seat_count 0으로 초기화
+                    seatCount.put(currentChairPos, 0);
                 } else {
                     // 사석화 O
                     for (Box other: others) {
-                        if (boxUtil.isColliding(chair, other)) {
+                        if (boxUtil.overlap_percent(chair, other) > boxCollidingPercentThreshold) {
                             // 사석화 O. seat_count += 1
+                            seatCount.put(currentChairPos, seatCount.get(currentChairPos) + 1);
                             break;
                         } else {
                             // 사석화 X. seat_count 0으로 초기화
+                            seatCount.put(currentChairPos, 0);
                         }
                     }
                 }
             }
         }
+        log.info("[AFTER] seatCount : " + seatCount.toString());
     }
 }
