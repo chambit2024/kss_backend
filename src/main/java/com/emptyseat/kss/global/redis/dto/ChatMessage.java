4328384f6b2df25eff293d942241c2a96f4a64cf
SeungHo0422 <songseungho9258@gmail.com>
2024-06-10 05:13:21 +0900

    fix: change request dto types

--- src/main/java/com/emptyseat/kss/global/redis/dto/ChatMessage.java
+++ src/main/java/com/emptyseat/kss/global/redis/dto/ChatMessage.java
@@ -8,6 +8,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChatMessage {
-    private String sender;
-    private String context;
+    private String filePath;
 }
