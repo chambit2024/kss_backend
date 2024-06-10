212ae959a8358488c5d6deaf9cc5b900f63b49f0
SeungHo0422 <songseungho9258@gmail.com>
2024-06-10 21:19:25 +0900

    fix: deprecated spring-security

--- src/main/java/com/emptyseat/kss/global/config/SecurityConfig.java
+++ src/main/java/com/emptyseat/kss/global/config/SecurityConfig.java
@@ -1,37 +1,37 @@
-package com.emptyseat.kss.global.config;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.DefaultSecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
-public class SecurityConfig {
-
-    private static final String[] PERMIT_URLS = {
-            /* apis */
-            "/api/**"
-    };
-
-//    private final JwtTokenProvider jwtTokenProvider;
-
-    @Bean
-    public DefaultSecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .cors(Customizer.withDefaults())
-                .csrf(csrf -> csrf.disable())
-                .httpBasic(httpBasic -> httpBasic.disable())
-                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(authorize -> authorize.requestMatchers(PERMIT_URLS).permitAll().anyRequest().authenticated());
-//                .exceptionHandling(exceptionConfig -> exceptionConfig.authenticationEntryPoint(new CustomAuthenticationEntryPoint()))
-//                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
-//                .addFilterBefore(new JwtExceptionFilter(), JwtAuthenticationFilter.class);
-        return http.build();
-    }
-}
+//package com.emptyseat.kss.global.config;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.DefaultSecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+//public class SecurityConfig {
+//
+//    private static final String[] PERMIT_URLS = {
+//            /* apis */
+//            "/api/**"
+//    };
+//
+////    private final JwtTokenProvider jwtTokenProvider;
+//
+//    @Bean
+//    public DefaultSecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .cors(Customizer.withDefaults())
+//                .csrf(csrf -> csrf.disable())
+//                .httpBasic(httpBasic -> httpBasic.disable())
+//                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .authorizeHttpRequests(authorize -> authorize.requestMatchers(PERMIT_URLS).permitAll().anyRequest().authenticated());
+////                .exceptionHandling(exceptionConfig -> exceptionConfig.authenticationEntryPoint(new CustomAuthenticationEntryPoint()))
+////                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+////                .addFilterBefore(new JwtExceptionFilter(), JwtAuthenticationFilter.class);
+//        return http.build();
+//    }
+//}
